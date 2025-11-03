4cc546920736d0c7fca250a209948a3e74504d82
bwng0v0 <ehs4742@gmail.com>
2025-11-03 20:56:38 +0900

    feat: 페이지 이동시 스크롤 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
 import { ApplicationPage } from './pages/ApplicationPage';
 import { ActivitiesPage } from './pages/ActivitiesPage';
+import ScrollToTop from './components/ScrollToTop';
 export function App() {
-  return <BrowserRouter>
+  return (
+    <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/apply" element={<ApplicationPage />} />
         <Route path="/activities" element={<ActivitiesPage />} />
       </Routes>
-    </BrowserRouter>;
+    </BrowserRouter>
+  );
 }
